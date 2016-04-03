5dc7b9e6d58ff0aee770cebd1c61f46827702ff9
Matt Bachmann <bachmann.matt@gmail.com>
2016-04-03 00:31:43 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+# You need to install https://github.com/Bachmann1234/CMS50Dplus
+setup(
+    name='heart-rate-hud',
+    version='1',
+    author='Matt Bachmann',
+    url='https://github.com/Bachmann1234/heart-rate-hud',
+    description='Tkinter window to display heartrate',
+    packages=find_packages()
+)
